a9d124326dcaffff6072fb21e35e766bed0d9f1d
Luiza Kill <134641408+killluiza@users.noreply.github.com>
2023-06-22 16:35:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+import { know, multiplica } from "./utiliratarios.js";
+
+know();
---
+++ utiliratarios.js
@@ -0,0 +1,11 @@
+const valor = 5;
+
+const know = function() {
+    alert("i realy don't know");
+}
+
+const multiplica = function(x) {
+     alert("x * valor");
+}
+
+export { know, multiplica };
